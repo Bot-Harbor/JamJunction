d17010a4c3886baafe6685ad2c4b099e0825e4e2
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-06 17:04:33 -0500

    Added error embed builders

---
+++ JamJunction.App/EmbedBuilders/ErrorEmbed.cs
@@ -0,0 +1,51 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace JamJunction.App.EmbedBuilders;
+
+public class ErrorEmbed
+{
+    public DiscordEmbedBuilder CommandFailedEmbedBuilder()
+    {
+        var commandErrorEmbed = new DiscordEmbedBuilder
+        {
+            Description = "⚠️ • Command failed to execute!",
+            Color = DiscordColor.Red
+        };
+
+        return commandErrorEmbed;
+    }
+
+    public DiscordEmbedBuilder VoiceChannelErrorEmbedBuilder(InteractionContext context)
+    {
+        var voiceChannelErrorEmbed = new DiscordEmbedBuilder
+        {
+            Description = $"🔊 • You must be in a voice channel **{context.Member.Username}**!",
+            Color = DiscordColor.Red
+        };
+
+        return voiceChannelErrorEmbed;
+    }
+
+    public DiscordEmbedBuilder LavaLinkErrorEmbedBuilder()
+    {
+        var lavaLinkErrorEmbed = new DiscordEmbedBuilder
+        {
+            Description = "🌋 🔗• Lavalink is not connected!",
+            Color = DiscordColor.Red
+        };
+
+        return lavaLinkErrorEmbed;
+    }
+
+    public DiscordEmbedBuilder AudioTrackErrorEmbedBuilder()
+    {
+        var audioTrackErrorEmbed = new DiscordEmbedBuilder
+        {
+            Description = $"🎵 • Failed to find song!",
+            Color = DiscordColor.Red
+        };
+
+        return audioTrackErrorEmbed;
+    }
+}
