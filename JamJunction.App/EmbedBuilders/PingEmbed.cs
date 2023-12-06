14e04f656e669f0abee5c4ada51a439962edb41e
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-06 17:04:50 -0500

    Added a ping embed builder

---
+++ JamJunction.App/EmbedBuilders/PingEmbed.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace JamJunction.App.EmbedBuilders;
+
+public class PingEmbed
+{
+    public DiscordEmbedBuilder PingEmbedBuilder(InteractionContext context)
+    {
+        var pingEmbed = new DiscordEmbedBuilder()
+        {
+            Title = $"Pong 🏓 ``{context.Member.Username}``",
+            ImageUrl = "https://pbs.twimg.com/media/CijH1M7WgAE_3we.jpg",
+            Color = DiscordColor.Orange,
+        };
+
+        var easternTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
+            TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+
+        pingEmbed.WithFooter
+        (
+            $"Time Stamp: {easternTime.ToString($"MMMM dd, yyyy h:mm tt")}"
+        );
+
+        return pingEmbed;
+    }
+}
