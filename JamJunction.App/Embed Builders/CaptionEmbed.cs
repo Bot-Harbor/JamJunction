da43c57c80569dcaba5de525a7e422f33e55bf79
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-06 19:54:49 -0500

    Created a caption command

---
+++ JamJunction.App/Embed Builders/CaptionEmbed.cs
@@ -0,0 +1,58 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Enums;
+
+namespace JamJunction.App.Embed_Builders;
+
+public class CaptionEmbed
+{
+    public DiscordEmbedBuilder CaptionEmbedBuilder(string caption, DiscordAttachment image, Color color)
+    {
+        var discordColor = color switch
+        {
+            Color.Aquamarine => DiscordColor.Aquamarine,
+            Color.Azure => DiscordColor.Azure,
+            Color.Black => DiscordColor.Black,
+            Color.Blue => DiscordColor.Blue,
+            Color.Bplurple => DiscordColor.Blurple,
+            Color.Brown => DiscordColor.Brown,
+            Color.Chartreuse => DiscordColor.Chartreuse,
+            Color.Cyan => DiscordColor.Cyan,
+            Color.DarkBlue => DiscordColor.DarkBlue,
+            Color.DarkGray => DiscordColor.DarkGray,
+            Color.DarkGreen => DiscordColor.DarkGreen,
+            Color.DarkRed => DiscordColor.DarkRed,
+            Color.Gold => DiscordColor.Gold,
+            Color.Goldenrod => DiscordColor.Goldenrod,
+            Color.Gray => DiscordColor.Gray,
+            Color.Green => DiscordColor.Green,
+            Color.LightGray => DiscordColor.LightGray,
+            Color.Orange => DiscordColor.Orange,
+            Color.Pink => DiscordColor.HotPink,
+            Color.Purple => DiscordColor.Purple,
+            Color.Red => DiscordColor.Red,
+            Color.SapGreen => DiscordColor.SapGreen,
+            Color.Teal => DiscordColor.Teal,
+            Color.White => DiscordColor.White,
+            Color.Yellow => DiscordColor.Yellow,
+            _ => DiscordColor.None
+        };
+
+        var captionEmbed = new DiscordEmbedBuilder()
+        {
+            Title = caption,
+            ImageUrl = image.Url,
+            Color = discordColor
+        };
+
+        var easternTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
+            TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+
+        captionEmbed.WithFooter
+        (
+            $"Time Stamp: {easternTime.ToString($"MMMM dd, yyyy h:mm tt")}"
+        );
+
+        return captionEmbed;
+    }
+}
---
+++ JamJunction.App/Slash Commands/CaptionCommand.cs
@@ -0,0 +1,34 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Embed_Builders;
+using JamJunction.App.Enums;
+
+namespace JamJunction.App.Slash_Commands;
+
+public class CaptionCommand : ApplicationCommandModule
+{
+    [SlashCommand("caption", "Give any image a caption.")]
+    public async Task CaptionAsync
+    (
+        InteractionContext context,
+        [Option("caption", "he caption you want the image to have.")]
+        string caption,
+        [Option("image", "The image you want to upload.")]
+        DiscordAttachment image,
+        [Option("color", "Pick the color you want the message embed to have.")] Color color
+    )
+    {
+        try
+        {
+            var captionEmbed = new CaptionEmbed();
+
+            await context.CreateResponseAsync(captionEmbed.CaptionEmbedBuilder(caption, image, color));
+        }
+        catch (FormatException)
+        {
+            var errorEmbed = new ErrorEmbed();
+
+            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), ephemeral: true);
+        }
+    }
+}
