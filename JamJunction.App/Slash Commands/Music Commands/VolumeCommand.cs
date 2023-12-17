6b77740bf3de34ad54331524329b32080adea5bc
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-17 14:57:41 -0500

    Added a volume command

---
+++ JamJunction.App/Slash Commands/Music Commands/VolumeCommand.cs
@@ -0,0 +1,78 @@
+﻿using DSharpPlus;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Embed_Builders;
+
+namespace JamJunction.App.Slash_Commands.Music_Commands;
+
+public class VolumeCommand : ApplicationCommandModule
+{
+    [SlashCommand("volume", "Adjust the volume 0-200.")]
+    public async Task VolumeCommandAsync(InteractionContext context,
+        [Option("level", "How loud do you want the music to be? Note: Must have \"manage channels\" permission.")]
+        double volume)
+    {
+        var errorEmbed = new ErrorEmbed();
+        var audioEmbed = new AudioPlayerEmbed();
+
+        try
+        {
+            var userVc = context.Member?.VoiceState?.Channel;
+            var lava = context.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+
+            if (context.Member != null && (context.Member.Permissions & Permissions.ManageChannels) != 0)
+            {
+                if (!lava.ConnectedNodes.Any())
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
+                }
+
+                if (userVc == null || userVc.Type != ChannelType.Voice)
+                {
+                    await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
+                }
+
+                await node.ConnectAsync(userVc);
+
+                var connection = node.GetGuildConnection(context.Guild);
+
+                if (connection == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
+                }
+
+                if (connection != null && connection.CurrentState.CurrentTrack == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoAudioTrackErrorEmbedBuilder());
+                }
+                
+                if (volume > 200)
+                {
+                    await context.CreateResponseAsync(audioEmbed.MaxVolumeEmbedBuilder(context));
+                }
+                else if (volume < 0)
+                {
+                    await context.CreateResponseAsync(audioEmbed.MinVolumeEmbedBuilder(context));
+                }
+                else
+                {
+                    if (connection != null)
+                    {
+                        await connection.SetVolumeAsync(Convert.ToInt32(volume));
+                        await context.CreateResponseAsync(audioEmbed.VolumeEmbedBuilder(Convert.ToInt32(volume),
+                            context));
+                    }
+                }
+            }
+            else
+            {
+                await context.CreateResponseAsync(errorEmbed.NoVolumePermissionEmbedBuilder());
+            }
+        }
+        catch (Exception e)
+        {
+            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), ephemeral: true);
+        }
+    }
+}
