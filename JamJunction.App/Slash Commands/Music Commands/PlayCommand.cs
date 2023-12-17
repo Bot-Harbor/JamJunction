a85fe36897877d1028eba7e3149681e90e58406d
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-17 14:56:43 -0500

    Added a play command

---
+++ JamJunction.App/Slash Commands/Music Commands/PlayCommand.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.Contracts;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Embed_Builders;
+
+namespace JamJunction.App.Slash_Commands.Music_Commands;
+
+public class PlayCommand : ApplicationCommandModule
+{
+    [SlashCommand("play", "Queue a song.")]
+    public async Task PlayAsync
+    (
+        InteractionContext context,
+        [Option("Song", "Enter the name of the song you want to queue.")]
+        string query
+    )
+    {
+        var errorEmbed = new ErrorEmbed();
+        var audioEmbed = new AudioPlayerEmbed();
+
+        try
+        {
+            var userVc = context.Member?.VoiceState?.Channel;
+            var lava = context.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+
+            if (context.Member != null && (context.Member.Permissions & Permissions.ManageChannels) != 0)
+            {
+                if (!lava.ConnectedNodes.Any())
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
+                }
+
+                if (userVc == null || userVc.Type != ChannelType.Voice)
+                {
+                    await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
+                }
+
+                await node.ConnectAsync(userVc);
+
+                var connection = node.GetGuildConnection(context.Guild);
+
+                if (connection == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
+                }
+
+                var loadResult = await node.Rest.GetTracksAsync(query);
+
+                if (loadResult.LoadResultType == LavalinkLoadResultType.LoadFailed
+                    || loadResult.LoadResultType == LavalinkLoadResultType.NoMatches)
+                {
+                    await context.CreateResponseAsync(errorEmbed.AudioTrackErrorEmbedBuilder());
+                }
+
+                var track = loadResult.Tracks.First();
+                
+                if (connection != null)
+                {
+                    await connection.PlayAsync(track);
+                    await context.CreateResponseAsync(audioEmbed.PlayEmbedBuilder(track));
+                }
+            }
+            else
+            {
+                await context.CreateResponseAsync(errorEmbed.NoPlayPermissionEmbedBuilder());
+            }
+        }
+        catch (Exception e)
+        {
+            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), ephemeral: true);
+        }
+    }
+}
