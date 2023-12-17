11a0d7ec479aecb52ffc680cd308c99e61c758f3
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-17 14:57:48 -0500

    Added a seek command

---
+++ JamJunction.App/Slash Commands/Music Commands/SeekCommand.cs
@@ -0,0 +1,68 @@
+﻿using DSharpPlus;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Embed_Builders;
+
+namespace JamJunction.App.Slash_Commands.Music_Commands;
+
+public class SeekCommand : ApplicationCommandModule
+{
+    [SlashCommand("seek", "Sets the position of the song.")]
+    public async Task SeekCommandAsync(InteractionContext context,
+        [Option("time", "Change the current position " +
+                        "of the song in seconds")]
+        double time)
+    {
+        var errorEmbed = new ErrorEmbed();
+        var audioEmbed = new AudioPlayerEmbed();
+
+        try
+        {
+            var userVc = context.Member?.VoiceState?.Channel;
+            var lava = context.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+
+            if (context.Member != null && (context.Member.Permissions & Permissions.ManageChannels) != 0)
+            {
+                if (!lava.ConnectedNodes.Any())
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
+                }
+
+                if (userVc == null || userVc.Type != ChannelType.Voice)
+                {
+                    await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
+                }
+
+                await node.ConnectAsync(userVc);
+
+                var connection = node.GetGuildConnection(context.Guild);
+
+                if (connection == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
+                }
+
+                if (connection != null && connection.CurrentState.CurrentTrack == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoAudioTrackErrorEmbedBuilder());
+                }
+
+                if (connection != null)
+                {
+                    await connection.ResetEqualizerAsync();
+                    await connection.SeekAsync(TimeSpan.FromSeconds(Math.Floor(time)));
+                    await context.CreateResponseAsync(audioEmbed.SeekEmbedBuilder(context, Math.Floor(time)));
+                }
+            }
+            else
+            {
+                await context.CreateResponseAsync(errorEmbed.NoSeekPermissionEmbedBuilder());
+            }
+        }
+        catch (Exception e)
+        {
+            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), ephemeral: true);
+        }
+    }
+}
