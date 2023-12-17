4f0bbf128cfb24275312fec474a9820860ac0673
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-17 14:58:48 -0500

    Registered music commands

--- JamJunction.App/Bot.cs
+++ JamJunction.App/Bot.cs
@@ -1,8 +1,13 @@
 ﻿using DSharpPlus;
 using DSharpPlus.EventArgs;
+using DSharpPlus.Lavalink;
+using DSharpPlus.Net;
 using DSharpPlus.SlashCommands;
 using JamJunction.App.Secrets;
 using JamJunction.App.Slash_Commands;
+using JamJunction.App.Slash_Commands.Music_Commands;
+using JamJunction.App.Slash_Commands.Other_Commands;
+using Microsoft.Extensions.Logging;
 
 namespace JamJunction.App;
 
@@ -19,16 +24,35 @@ public abstract class Bot
             Intents = DiscordIntents.All,
             Token = discord.BotToken,
             TokenType = TokenType.Bot,
-            AutoReconnect = true
+            AutoReconnect = true,
+            MinimumLogLevel = LogLevel.Debug
+        };
+        
+        var endpoint = new ConnectionEndpoint
+        {
+            Hostname = discord.HostName,
+            Port = discord.Port,
+            Secured = true
         };
 
+        var lavaLinkConfig = new LavalinkConfiguration
+        {
+            Password = discord.Password,
+            RestEndpoint = endpoint,
+            SocketEndpoint = endpoint,
+        };
+        
         Client = new DiscordClient(discordConfig);
 
         Client.Ready += Client_Ready;
         
         SlashCommands();
+        
+        var lavaLink = Client.UseLavalink();
 
         await Client.ConnectAsync();
+        await lavaLink.ConnectAsync(lavaLinkConfig);
+        
         await Task.Delay(-1);
     }
     
@@ -37,10 +61,17 @@ public abstract class Bot
         return Task.CompletedTask;
     }
 
-    public static void SlashCommands()
+    private static void SlashCommands()
     {
         var slashCommands = Client.UseSlashCommands();
         
         slashCommands.RegisterCommands<PingCommand>();
+        slashCommands.RegisterCommands<CaptionCommand>();
+        slashCommands.RegisterCommands<PlayCommand>();
+        slashCommands.RegisterCommands<PauseCommand>();
+        slashCommands.RegisterCommands<ResumeCommand>();
+        slashCommands.RegisterCommands<StopCommand>();
+        slashCommands.RegisterCommands<VolumeCommand>();
+        slashCommands.RegisterCommands<SeekCommand>();
     }
 }
