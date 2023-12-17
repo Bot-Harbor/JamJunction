061b70438a5c5f2cc66129175b438cb6d52c726e
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-17 14:56:55 -0500

    Added a pause command

---
+++ JamJunction.App/Slash Commands/Music Commands/PauseCommand.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus;
+using DSharpPlus.Lavalink;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Embed_Builders;
+
+namespace JamJunction.App.Slash_Commands.Music_Commands;
+
+public class PauseCommand : ApplicationCommandModule
+{
+    [SlashCommand("pause", "Pauses the current song.")]
+    public async Task PauseCommandAsync(InteractionContext context)
+    {
+        var errorEmbed = new ErrorEmbed();
+        var audioEmbed = new AudioPlayerEmbed();
+
+        try
+        {
+            var userVc = context.Member?.VoiceState?.Channel;
+            var lava = context.Client.GetLavalink();
+            var node = lava.ConnectedNodes.Values.First();
+            
+            if (context.Member != null && (context.Member.Permissions & Permissions.ManageChannels) != 0)
+            {
+                if (!lava.ConnectedNodes.Any())
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
+                }
+            
+                if (userVc == null || userVc.Type != ChannelType.Voice)
+                {
+                    await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
+                }
+                
+                await node.ConnectAsync(userVc);
+                
+                var connection = node.GetGuildConnection(context.Guild);
+
+                if (connection == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
+                }
+
+                if (connection != null && connection.CurrentState.CurrentTrack == null)
+                {
+                    await context.CreateResponseAsync(errorEmbed.NoAudioTrackErrorEmbedBuilder());
+                }
+
+                if (connection != null)
+                {
+                    await connection.PauseAsync();
+                    await context.CreateResponseAsync(audioEmbed.PauseEmbedBuilder(context));
+                }
+            }
+            else
+            {
+                await context.CreateResponseAsync(errorEmbed.NoPausePermissionEmbedBuilder());
+            }
+        }
+        catch (Exception e)
+        {
+            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), ephemeral: true);
+        }
+    }
+}
