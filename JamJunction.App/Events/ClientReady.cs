36d6e84b2fca768775747a7d864e20ab57be8267
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-19 21:25:56 -0500

    Moved event to events directory

---
+++ JamJunction.App/Events/ClientReady.cs
@@ -0,0 +1,12 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace JamJunction.App.Events;
+
+public class ClientReady
+{
+    public static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
+}
