971b81904d472ead40deb37b9311309c54176c75
Kaleb Garrett <kalebagarrett@gmail.com>
2023-12-21 15:29:59 -0500

    Added interface and button handler for button events

---
+++ JamJunction.App/Events/Buttons/ButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
+using JamJunction.App.Interfaces;
+
+namespace JamJunction.App.Events.Buttons;
+
+public class ButtonHandler
+{
+    /// <summary>
+    /// Execute Button Event
+    /// </summary>
+    /// <param name="button"></param>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    public static Task Execute(IButton button, DiscordClient sender, ComponentInteractionCreateEventArgs e)
+    {
+        return button.Execute(sender, e);
+    }
+}
---
+++ JamJunction.App/Interfaces/IButton.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.SlashCommands;
+
+namespace JamJunction.App.Interfaces;
+
+public interface IButton
+{
+    Task Execute(DiscordClient sender, ComponentInteractionCreateEventArgs e);
+}
