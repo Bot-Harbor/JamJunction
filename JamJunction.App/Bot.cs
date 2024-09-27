0b36035d050463631e49dbac2dde6173e7aa45d4
Kaleb Garrett <kalebagarrett@gmail.com>
2024-09-27 17:14:47 -0400

    Reconfigured bot to use host application builder and Lavalink4Net

--- JamJunction.App/Bot.cs
+++ JamJunction.App/Bot.cs
@@ -1,73 +1,39 @@
 ﻿using DSharpPlus;
-using DSharpPlus.Lavalink;
-using Lavalink4NET;
-using DSharpPlus.Net;
 using DSharpPlus.SlashCommands;
-using JamJunction.App.Events;
 using JamJunction.App.Events.Buttons;
-using JamJunction.App.Secrets;
 using JamJunction.App.Slash_Commands.Music_Commands;
 using JamJunction.App.Slash_Commands.Other_Commands;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace JamJunction.App;
 
-public abstract class Bot
+public class Bot : BackgroundService
 {
     public static readonly Dictionary<ulong, AudioPlayerController> GuildAudioPlayers = new();
-    public static DiscordClient Client { get; set; }
 
-    public static async Task RunBotAsync()
+    private readonly IServiceProvider _serviceProvider;
+    private readonly DiscordClient _discordClient;
+
+    public Bot(IServiceProvider serviceProvider, DiscordClient discordClient)
     {
-        var discord = new DiscordSecrets();
-
-        var discordConfig = new DiscordConfiguration
-        {
-            Intents = DiscordIntents.All,
-            Token = discord.BotToken,
-            TokenType = TokenType.Bot,
-            AutoReconnect = true,
-            MinimumLogLevel = LogLevel.Debug
-        };
-
-        var endpoint = new ConnectionEndpoint
-        {
-            Hostname = discord.HostName,
-            Port = discord.Port,
-            Secured = false
-        };
-
-        var lavaLinkConfig = new LavalinkConfiguration
-        {
-            Password = discord.Password,
-            RestEndpoint = endpoint,
-            SocketEndpoint = endpoint
-        };
-
-        Client = new DiscordClient(discordConfig);
-
-        var lavaLink = Client.UseLavalink();
-
-        await Client.ConnectAsync();
-        
-        Client.VoiceStateUpdated += VoiceStateUpdated.ClientOnVoiceStateUpdated;
-        
-        await lavaLink.ConnectAsync(lavaLinkConfig);
-        var nodeConnection = lavaLink.GetNodeConnection(endpoint);
-        
-        nodeConnection.GuildConnectionCreated += GuildConnectionCreated.NodeConnectionOnGuildConnectionCreated;
-        nodeConnection.TrackStuck += TrackStuck.TrackIsStuck;
-        nodeConnection.PlaybackFinished += PlayBackFinished.PlayBackIsFinished;
-
-        ButtonEvents();
-        SlashCommands();
-
-        await Task.Delay(-1);
+        _serviceProvider = serviceProvider;
+        _discordClient = discordClient;
     }
 
-    private static void SlashCommands()
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var slashCommands = Client.UseSlashCommands();
+        await _discordClient
+            .ConnectAsync()
+            .ConfigureAwait(false);
+        
+        ConfigSlashCommands();
+        ButtonEvents();
+    }
+
+    private void ConfigSlashCommands()
+    {
+        var slashCommands =
+            _discordClient.UseSlashCommands(new SlashCommandsConfiguration {Services = _serviceProvider});
 
         slashCommands.RegisterCommands<PingCommand>();
         slashCommands.RegisterCommands<CaptionCommand>();
@@ -89,9 +55,9 @@ public abstract class Bot
         slashCommands.RegisterCommands<SongPositionCommand>();
     }
 
-    private static void ButtonEvents()
+    private void ButtonEvents()
     {
-        Client.ComponentInteractionCreated += async (sender, args) =>
+        _discordClient.ComponentInteractionCreated += async (sender, args) =>
         {
             await ButtonHandler.Execute(new PauseButton(), sender, args);
             await ButtonHandler.Execute(new ResumeButton(), sender, args);
--- JamJunction.App/Program.cs
+++ JamJunction.App/Program.cs
@@ -1,3 +1,35 @@
-﻿using JamJunction.App;
+﻿using DSharpPlus;
+using JamJunction.App;
+using JamJunction.App.Secrets;
+using Lavalink4NET;
+using Lavalink4NET.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-await Bot.RunBotAsync();
+var builder = new HostApplicationBuilder();
+builder.Services.AddHostedService<Bot>();
+builder.Services.AddSingleton<DiscordClient>();
+
+builder.Services.AddSingleton(new DiscordConfiguration
+{
+    TokenType = TokenType.Bot,
+    Token = DiscordSecrets.BotToken,
+    Intents = DiscordIntents.All,
+    MinimumLogLevel = LogLevel.Information,
+    AutoReconnect = true,
+});
+
+builder.Services.ConfigureLavalink(config =>
+{
+    config.HttpClientName = DiscordSecrets.HostName;
+    config.Passphrase = DiscordSecrets.Password;
+    config.ResumptionOptions = new LavalinkSessionResumptionOptions(TimeSpan.FromSeconds(15));
+    config.ReadyTimeout = TimeSpan.FromSeconds(15);
+});
+
+builder.Services.AddLavalink();
+
+builder.Services.AddLogging(s => s.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+builder.Build().Run();
