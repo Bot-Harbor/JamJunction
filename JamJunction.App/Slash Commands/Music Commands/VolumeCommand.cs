345604d54d2cac7284385e76a62da937ccea74f8
Kaleb Garrett <kalebagarrett@gmail.com>
2024-10-03 19:09:58 -0400

    Reconfigured volume command

--- JamJunction.App/Slash Commands/Music Commands/VolumeCommand.cs
+++ JamJunction.App/Slash Commands/Music Commands/VolumeCommand.cs
@@ -1,73 +1,111 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.Lavalink;
 using DSharpPlus.SlashCommands;
 using JamJunction.App.Embed_Builders;
+using Lavalink4NET;
 
 namespace JamJunction.App.Slash_Commands.Music_Commands;
 
 public class VolumeCommand : ApplicationCommandModule
 {
-    [SlashCommand("volume", "Adjust the volume 0-100. Default volume is 50.")]
+    private readonly IAudioService _audioService;
+
+    public VolumeCommand(IAudioService audioService)
+    {
+        _audioService = audioService;
+    }
+
+    [SlashCommand("volume", "Adjust the volume 0-100. Default volume is 100.")]
     public async Task VolumeCommandAsync(InteractionContext context,
         [Option("level", "How loud do you want the music to be?")]
         double volume)
     {
+        await context.DeferAsync();
+
+        var audioPlayerEmbed = new AudioPlayerEmbed();
         var errorEmbed = new ErrorEmbed();
-        var audioEmbed = new AudioPlayerEmbed();
 
-        try
+        var guildId = context.Guild.Id;
+        var userVoiceChannel = context.Member?.VoiceState?.Channel;
+
+        if (userVoiceChannel == null)
         {
-            var userVc = context.Member?.VoiceState?.Channel;
-            var lava = context.Client.GetLavalink();
-            var node = lava.ConnectedNodes.Values.First();
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context)));
 
-            if (!lava.ConnectedNodes!.Any())
-                await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
-
-            if (userVc == null || userVc.Type != ChannelType.Voice)
-                await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
-
-            var connection = node.GetGuildConnection(context.Guild);
-
-            if (connection! == null) await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
-
-            if (connection != null && connection.CurrentState.CurrentTrack == null)
-                await context.CreateResponseAsync(errorEmbed.NoAudioTrackErrorEmbedBuilder());
-
-            if (connection != null)
-            {
-                var guildId = context.Guild.Id;
-                var audioPlayerController = Bot.GuildAudioPlayers[guildId];
-
-                if (audioPlayerController.PauseInvoked)
-                {
-                    await context.CreateResponseAsync(errorEmbed.NoVolumeWhilePausedEmbedBuilder(context));
-                }
-                else
-                {
-                    if (volume > 100)
-                    {
-                        await context.CreateResponseAsync(errorEmbed.MaxVolumeEmbedBuilder(context));
-                    }
-                    else if (volume < 0)
-                    {
-                        await context.CreateResponseAsync(errorEmbed.MinVolumeEmbedBuilder(context));
-                    }
-                    else
-                    {
-                        await connection.SetVolumeAsync(Convert.ToInt32(volume));
-                        await context.CreateResponseAsync(audioEmbed.VolumeEmbedBuilder(Convert.ToInt32(volume),
-                            context));
-
-                        audioPlayerController.Volume = Convert.ToInt32(volume);
-                        audioPlayerController.MuteInvoked = false;
-                    }
-                }
-            }
+            return;
         }
-        catch (Exception e)
+
+        var botId = context.Client.CurrentUser.Id;
+        var botVoiceChannel = context.Guild.VoiceStates.TryGetValue(botId, out var botVoiceState);
+
+        if (botVoiceChannel == false)
         {
-            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), true);
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.NoPlayerErrorEmbedBuilder()));
+
+            return;
         }
+
+        if (userVoiceChannel.Id != botVoiceState.Channel!.Id)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.SameVoiceChannelErrorEmbedBuilder(context)));
+
+            return;
+        }
+
+        var lavalinkPlayer = new LavalinkPlayerHandler(_audioService);
+        var player =
+            await lavalinkPlayer.GetPlayerAsync(context, guildId, userVoiceChannel, connectToVoiceChannel: false);
+
+        if (player == null)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.NoConnectionErrorEmbedBuilder()));
+
+            return;
+        }
+
+        if (player!.CurrentTrack == null)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.NoAudioTrackErrorEmbedBuilder()));
+
+            return;
+        }
+        
+        var isInt = volume == (int) volume;
+
+        if (!isInt)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.VolumeNotAnIntegerEmbedBuilder(context)));
+            
+            return;
+        }
+
+        if (volume > 100)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.MaxVolumeEmbedBuilder(context)));
+
+            return;
+        }
+
+        volume /= 100;
+        await player!.SetVolumeAsync((float) volume);
+
+        await context.FollowUpAsync(
+            new DiscordFollowupMessageBuilder().AddEmbed(
+                audioPlayerEmbed.VolumeEmbedBuilder(volume * 100, context)));
     }
 }
