8f17f19791a37db5322f658dfef6b2af0e92e8d2
Kaleb Garrett <kalebagarrett@gmail.com>
2024-10-03 15:56:51 -0400

    Reconfigured leave command

--- JamJunction.App/Slash Commands/Music Commands/LeaveCommand.cs
+++ JamJunction.App/Slash Commands/Music Commands/LeaveCommand.cs
@@ -1,44 +1,82 @@
-﻿using DSharpPlus;
+﻿using System.Data;
+using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.Lavalink;
 using DSharpPlus.SlashCommands;
 using JamJunction.App.Embed_Builders;
+using JamJunction.App.Slash_Commands.Music_Commands.Enums;
+using Lavalink4NET;
+using Lavalink4NET.Clients;
+using Lavalink4NET.Rest.Entities.Tracks;
 
 namespace JamJunction.App.Slash_Commands.Music_Commands;
 
 public class LeaveCommand : ApplicationCommandModule
 {
+    private readonly IAudioService _audioService;
+
+    public LeaveCommand(IAudioService audioService)
+    {
+        _audioService = audioService;
+    }
+
     [SlashCommand("leave", "Disconnects the player.")]
     public async Task LeaveCommandAsync(InteractionContext context)
     {
+        await context.DeferAsync();
+        
+        var audioPlayerEmbed = new AudioPlayerEmbed();
         var errorEmbed = new ErrorEmbed();
-        var audioEmbed = new AudioPlayerEmbed();
 
-        try
+        var guildId = context.Guild.Id;
+        var userVoiceChannel = context.Member?.VoiceState?.Channel;
+        
+        if (userVoiceChannel == null)
         {
-            var userVc = context.Member?.VoiceState?.Channel;
-            var lava = context.Client.GetLavalink();
-            var node = lava.ConnectedNodes.Values.First();
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context)));
 
-            if (!lava.ConnectedNodes!.Any())
-                await context.CreateResponseAsync(errorEmbed.NoConnectionErrorEmbedBuilder());
-
-            if (userVc == null || userVc.Type != ChannelType.Voice)
-                await context.CreateResponseAsync(errorEmbed.ValidVoiceChannelErrorEmbedBuilder(context));
-
-            var connection = node.GetGuildConnection(context.Guild);
-
-            if (connection! == null) await context.CreateResponseAsync(errorEmbed.LavaLinkErrorEmbedBuilder());
-
-            if (connection != null)
-            {
-                await connection.DisconnectAsync();
-
-                await context.CreateResponseAsync(audioEmbed.LeaveEmbedBuilder(context));
-            }
+            return;
         }
-        catch (Exception e)
+        
+        var botId = context.Client.CurrentUser.Id;
+        var botVoiceChannel = context.Guild.VoiceStates.TryGetValue(botId, out var botVoiceState);
+
+        if (botVoiceChannel == false)
         {
-            await context.CreateResponseAsync(errorEmbed.CommandFailedEmbedBuilder(), true);
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.NoPlayerErrorEmbedBuilder()));
+            
+            return;
         }
+        
+        if (userVoiceChannel.Id != botVoiceState.Channel!.Id)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.SameVoiceChannelErrorEmbedBuilder(context)));
+            
+            return;
+        }
+        
+        var lavalinkPlayer = new LavalinkPlayerHandler(_audioService);
+        var player = await lavalinkPlayer.GetPlayerAsync(context, guildId, userVoiceChannel, connectToVoiceChannel: false);
+
+        if (player == null)
+        {
+            await context.FollowUpAsync(
+                new DiscordFollowupMessageBuilder().AddEmbed(
+                    errorEmbed.NoConnectionErrorEmbedBuilder()));
+            
+            return;
+        }
+        
+        await player!.DisconnectAsync();
+
+        await context.FollowUpAsync(
+            new DiscordFollowupMessageBuilder().AddEmbed(
+                audioPlayerEmbed.LeaveEmbedBuilder(context)));
     }
 }
