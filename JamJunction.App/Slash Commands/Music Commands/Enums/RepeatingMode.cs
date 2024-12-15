5151d6d589b26dc6e5b3d9e85430cf14d02bf0b4
Kaleb Garrett <kalebagarrett@gmail.com>
2024-12-15 17:03:18 -0500

    Added enum for repeat modes

---
+++ JamJunction.App/Slash Commands/Music Commands/Enums/RepeatingMode.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace JamJunction.App.Slash_Commands.Music_Commands.Enums;
+
+public enum RepeatingMode
+{
+    [ChoiceName("None")] None,
+    [ChoiceName("Repeat Track")] RepeatTrack,
+    [ChoiceName("Repeat Queue")] RepeatQueue
+}
