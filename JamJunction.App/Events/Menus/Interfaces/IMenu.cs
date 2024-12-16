24fc97e9371615d3eabb9e6df7e60c259258ae7d
Kaleb Garrett <kalebagarrett@gmail.com>
2024-12-16 16:18:04 -0500

    Added interface and handler for Discord Menus

---
+++ JamJunction.App/Events/Menus/Interfaces/IMenu.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace JamJunction.App.Events.Menus.Interfaces;
+
+public interface IMenu
+{
+    Task Execute(DiscordClient sender, ComponentInteractionCreateEventArgs e);
+}
---
+++ JamJunction.App/Events/Menus/MenuHandler.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using JamJunction.App.Events.Menus.Interfaces;
+
+namespace JamJunction.App.Events.Menus;
+
+public class MenuHandler
+{
+    public Task Execute(IMenu menu, DiscordClient sender, ComponentInteractionCreateEventArgs btnInteractionArgs)
+    {
+        return menu.Execute(sender, btnInteractionArgs);
+    }
+}
