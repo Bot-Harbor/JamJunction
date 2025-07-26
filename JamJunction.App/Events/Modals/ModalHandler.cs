f9b675bed34fd91701c519facd1a46a25e2176a6
Kaleb Garrett <kalebagarrett@gmail.com>
2025-07-26 15:46:01 -0400

    Added handler for modals

---
+++ JamJunction.App/Events/Modals/ModalHandler.cs
@@ -0,0 +1,13 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using JamJunction.App.Events.Modals.Interfaces;
+
+namespace JamJunction.App.Events.Modals;
+
+public class ModalHandler
+{
+    public Task Execute(IModal modal, DiscordClient sender, ModalSubmitEventArgs modalEventArgs)
+    {
+        return modal.Execute(sender, modalEventArgs);
+    }
+}
