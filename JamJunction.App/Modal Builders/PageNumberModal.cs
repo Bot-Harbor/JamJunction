265999470a30ef2682153bcdf31f277d9bb80f71
Kaleb Garrett <kalebagarrett@gmail.com>
2025-07-25 21:35:16 -0400

    Created modal for page number selection

---
+++ JamJunction.App/Modal Builders/PageNumberModal.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.Entities;
+
+namespace JamJunction.App.Modal_Builders;
+
+public class PageNumberModal
+{
+    public DiscordInteractionResponseBuilder Build()
+    {
+        var model = new DiscordInteractionResponseBuilder()
+            .WithTitle("Jump To Page")
+            .WithCustomId("jump-to-page")
+            .AddComponents(
+                new TextInputComponent("Page Number", "page-number"));
+        
+        return model;
+    }
+}
