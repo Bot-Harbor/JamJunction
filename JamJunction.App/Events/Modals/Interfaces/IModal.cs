86619a135540533796b4b323b3f0896f263fe418
Kaleb Garrett <kalebagarrett@gmail.com>
2025-07-26 15:45:50 -0400

    Added interface for modals

---
+++ JamJunction.App/Events/Modals/Interfaces/IModal.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace JamJunction.App.Events.Modals.Interfaces;
+
+public interface IModal
+{
+    Task Execute(DiscordClient sender, ModalSubmitEventArgs modalEventArgs);
+}
