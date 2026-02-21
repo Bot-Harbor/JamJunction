7c372112702371d8567e148b6dc1d19da259568b
Kaleb Garrett <kalebagarrett@gmail.com>
2026-02-21 01:04:24 -0500

    Changed version number

--- JamJunction.App/Views/Embeds/HelpEmbed.cs
+++ JamJunction.App/Views/Embeds/HelpEmbed.cs
@@ -40,7 +40,7 @@ public class HelpEmbed
             Footer = new DiscordEmbedBuilder.EmbedFooter
             {
                 Text = "Bot Info  •  " +
-                       "Version: 2.5.3  •  " +
+                       "Version: 2.5.4  •  " +
                        $"Total Servers: {serverCount}  •  " +
                        $"Shard: {shardCount}  •  " +
                        $"Ping: {ping}"
