c14d54b9df6f0ceed3835adb116d5abb3910b341
Kaleb Garrett <kalebagarrett@gmail.com>
2026-04-13 19:51:12 -0400

    Add help menu

---
+++ JamJunction.App/Views/Menus/HelpMenu.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.Entities;
+
+namespace JamJunction.App.Views.Menus;
+
+public class HelpMenu
+{
+    public DiscordSelectComponent Build()
+    {
+        var options = new List<DiscordSelectComponentOption>
+        {
+            new("📜 Main Menu", "main"),
+            new("🌐 All Features", "all-features"),
+            new("🎵 Music Commands Explanation", "music-commands"),
+            new("🛠️ Other Commands Explanation", "other-commands"),
+            new("📻 Player Controls Explanation", "play-controls"),
+        };
+
+        var menu = new DiscordSelectComponent("help-menu", "Browse Help Menu", options);
+        return menu;
+    }
+}
